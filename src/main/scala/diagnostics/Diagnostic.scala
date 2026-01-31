3bb532d4472c58ccd732bb4fd3c9abe3fe8302e2
agamjeetsingh <agamjeetsingh2005@gmail.com>
2026-01-31 20:16:12 +0000

    feat: Implemented visibility and modality resolvers and a sink ~> operator

--- src/main/scala/diagnostics/Diagnostic.scala
+++ src/main/scala/diagnostics/Diagnostic.scala
@@ -1,5 +1,6 @@
 package diagnostics
 
+import ast.Modifier
 import sources.SourceRange
 
 enum Diagnostic(val msg: String, val range: SourceRange) {
@@ -10,4 +11,10 @@ enum Diagnostic(val msg: String, val range: SourceRange) {
   case ExpectedSomething(expected: String, found: String, override val range: SourceRange) extends Diagnostic(s"Expected '$expected' but found '$found'", range)
   
   case SymbolAlreadyDeclared(name: String, override val range: SourceRange) extends Diagnostic(s"Symbol '$name' has already been declared in the current scope", range)
+  
+  case IllegalModifierCombination(modifier1: Modifier, modifier2: Modifier, override val range: SourceRange) extends Diagnostic(s"Illegal combination of modifiers '${modifier1.prettyPrint}' and '${modifier2.prettyPrint}'", range)
+  
+  case RepeatedModifier(modifier: Modifier, override val range: SourceRange) extends Diagnostic(s"Repeated modifier '${modifier.prettyPrint}'", range)
+  
+  case ModifierNotAllowed(modifier: Modifier, override val range: SourceRange) extends Diagnostic(s"Modifier '${modifier.prettyPrint}' not allowed here", range)
 }
--- src/main/scala/symbols/SemanticAnalyser.scala
+++ src/main/scala/symbols/SemanticAnalyser.scala
@@ -1,8 +1,10 @@
 package symbols
 
-import ast.{ClassDecl, Decl, FunctionDecl, KahwaFile, TypeParameterDecl, VariableDecl}
+import ast.Modifier.{FINAL, PRIVATE, PROTECTED, PUBLIC}
+import ast.{ClassDecl, Decl, FunctionDecl, KahwaFile, Modifier, ModifierNode, TypeParameterDecl, VariableDecl}
 import diagnostics.Diagnostic
-import diagnostics.Diagnostic.SymbolAlreadyDeclared
+import diagnostics.Diagnostic.{IllegalModifierCombination, ModifierNotAllowed, RepeatedModifier, SymbolAlreadyDeclared}
+import parser.Token.Public
 import sources.SourceRange
 
 import scala.collection.mutable.ListBuffer
@@ -49,31 +51,41 @@ object SemanticAnalyser {
     register(parentSymbol, decls, declToSymbolAndRange, registerSymbol, duplicatesAllowed, false)
   }
 
-  private def declareFile(kahwaFile: KahwaFile): TranslationUnit = {
+  extension[T] (symbolAndDiagnostics: (T, Iterable[Diagnostic])) {
+    def ~>(sink: ListBuffer[Diagnostic]): T = {
+      sink ++= symbolAndDiagnostics._2
+      symbolAndDiagnostics._1
+    }
+  }
+
+  private def declareFile(kahwaFile: KahwaFile): (TranslationUnit, List[Diagnostic]) = {
     val translationUnit = TranslationUnit(kahwaFile.range.fileId.toString, List.empty)
+    val diagnostics = ListBuffer[Diagnostic]()
 
     registerType(
       translationUnit,
       kahwaFile.classDecls,
-      classDecl => (declareClass(classDecl, translationUnit.scope), SourceRange.dummy),
+      classDecl => (declareClass(classDecl, translationUnit.scope, true) ~> diagnostics, SourceRange.dummy),
       translationUnit.classes += _)
-    
-    translationUnit
+
+    (translationUnit, diagnostics.toList)
   }
 
-  private def declareClass(classDecl: ClassDecl, outerScope: Scope): ClassSymbol = {
+  private def declareClass(classDecl: ClassDecl, outerScope: Scope, topLevel: Boolean): (ClassSymbol, List[Diagnostic]) = {
     val classSymbol = ClassSymbol(classDecl.name, outerScope)
+    val diagnostics = ListBuffer[Diagnostic]()
 
     registerType(
       classSymbol,
       classDecl.typeParameters,
       (typeParameterDecl : TypeParameterDecl) => (TypeParameterSymbol(typeParameterDecl.name, classSymbol.scope, typeParameterDecl.variance), typeParameterDecl.range),
-      classSymbol.genericArguments += _)
+      classSymbol.genericArguments += _
+    )
 
     registerType(
       classSymbol,
       classDecl.nestedClasses,
-      nestedClassDecl => (declareClass(nestedClassDecl, classSymbol.scope), classDecl.range),
+      nestedClassDecl => (declareClass(nestedClassDecl, classSymbol.scope, false) ~> diagnostics, classDecl.range),
       classSymbol.nestedClasses += _
     )
 
@@ -91,8 +103,14 @@ object SemanticAnalyser {
       variableDecl => (declareField(variableDecl, classSymbol.scope), variableDecl.range),
       classSymbol.fields += _
     )
+    
+    classSymbol.visibility = resolveVisibility(classDecl.modifiers, topLevel) ~> diagnostics
 
-    classSymbol
+    diagnostics ++= modifierNotAllowed(classDecl.modifiers, Set(Modifier.STATIC, Modifier.OVERRIDE).contains)
+
+//    classSymbol.setModality(resolveModality(classDecl.modifiers) ~> diagnostics)
+
+    (classSymbol, diagnostics.toList)
   }
 
   private def declareMethod(functionDecl: FunctionDecl, outerScope: Scope): MethodSymbol = {
@@ -100,8 +118,111 @@ object SemanticAnalyser {
 
     methodSymbol
   }
-  
+
   private def declareField(variableDecl: VariableDecl, outerScope: Scope): FieldSymbol = {
     ???
+  }
+
+  private def resolveVisibility(allModifiers: List[ModifierNode], topLevel: Boolean): (Visibility, List[Diagnostic]) = {
+    val modifiers = allModifiers.filter(_.modifier.isVisibility)
+
+    val diagnostics = ListBuffer[Diagnostic]()
+
+    val res = if (modifiers.isEmpty) {
+      Visibility.PUBLIC
+    } else if (modifiers.size == 1) {
+      if (topLevel && modifiers.head.modifier == Modifier.PROTECTED) {
+        Visibility.PUBLIC
+      } else {
+        Visibility.fromModifier(modifiers.head.modifier)
+      }
+    } else {
+      Visibility.fromModifier(modifiers.head.modifier)
+    }
+
+    var publicFound = false
+    var protectedFound = false
+    var privateFound = false
+
+    modifiers.foreach(modifierNode => modifierNode.modifier match {
+      case Modifier.PUBLIC => {
+        if ((!topLevel && protectedFound) || privateFound) {
+          diagnostics += IllegalModifierCombination(if (protectedFound) PROTECTED else PRIVATE, PUBLIC, modifierNode.range)
+        } else if (publicFound) {
+          diagnostics += RepeatedModifier(PUBLIC, modifierNode.range)
+        }
+        publicFound = true
+      }
+      case Modifier.PRIVATE => {
+        if ((!topLevel && protectedFound) || publicFound) {
+          diagnostics += IllegalModifierCombination(if (protectedFound) PROTECTED else PRIVATE, PRIVATE, modifierNode.range)
+        } else if (privateFound) {
+          diagnostics += RepeatedModifier(PRIVATE, modifierNode.range)
+        }
+        privateFound = true
+      }
+      case Modifier.PROTECTED => {
+        if (topLevel) {
+          diagnostics += ModifierNotAllowed(PROTECTED, modifierNode.range)
+        } else if (publicFound || privateFound) {
+          diagnostics += IllegalModifierCombination(if (publicFound) PUBLIC else PRIVATE, PROTECTED, modifierNode.range)
+        } else if (protectedFound) {
+          diagnostics += RepeatedModifier(PROTECTED, modifierNode.range)
+        }
+        protectedFound = true
+      }
+      case _ => throw IllegalStateException("Should be unreachable")
+    })
+
+    (res, diagnostics.toList)
+  }
+
+  private def modifierNotAllowed(modifiers: List[ModifierNode], notAllowed: Modifier => Boolean): List[Diagnostic] = {
+    modifiers.collect(modifierNode => modifierNode.modifier match {
+      case modifier if notAllowed(modifier) => ModifierNotAllowed(modifier, modifierNode.range)
+    })
+  }
+
+  private def resolveModality(allModifiers: List[ModifierNode]): (Modifier, List[Diagnostic]) = {
+    val modifiers = allModifiers.filter(_.modifier.isModality)
+
+    val diagnostics = ListBuffer[Diagnostic]()
+
+    val res = if (modifiers.nonEmpty) {
+      if (modifiers.contains(Modifier.ABSTRACT)) {
+        Modifier.ABSTRACT
+      } else if (modifiers.contains(Modifier.OPEN)) {
+        Modifier.OPEN
+      } else {
+        Modifier.FINAL
+      }
+    } else {
+      Modifier.FINAL
+    }
+
+    var finalFound = false
+    var openFound = false
+    var abstractFound = false
+
+    modifiers.foreach(modifierNode => modifierNode.modifier match {
+      case Modifier.OPEN => {
+        if (finalFound) diagnostics += IllegalModifierCombination(Modifier.FINAL, Modifier.OPEN, modifierNode.range)
+        else if (openFound) diagnostics += RepeatedModifier(Modifier.OPEN, modifierNode.range)
+        openFound = true
+      }
+      case Modifier.FINAL => {
+        if (abstractFound || openFound) diagnostics += IllegalModifierCombination(if (abstractFound) Modifier.ABSTRACT else Modifier.OPEN, Modifier.FINAL, modifierNode.range)
+        else if (finalFound) diagnostics += RepeatedModifier(Modifier.FINAL, modifierNode.range)
+        finalFound = true
+      }
+      case Modifier.ABSTRACT => {
+        if (finalFound) diagnostics += IllegalModifierCombination(Modifier.FINAL, Modifier.ABSTRACT, modifierNode.range)
+        else if (abstractFound) diagnostics += RepeatedModifier(Modifier.ABSTRACT, modifierNode.range)
+        abstractFound = true
+      }
+      case _ => throw IllegalStateException("Should be unreachable")
+    })
+
+    (res, diagnostics.toList)
   }
 }
