c64f36d521d8e3e497743f5477ca1196672df16a
agamjeetsingh <agamjeetsingh2005@gmail.com>
2026-01-31 12:43:45 +0000

    feat: Added diagnostic for unrecognised token

--- src/main/scala/diagnostics/Diagnostic.scala
+++ src/main/scala/diagnostics/Diagnostic.scala
@@ -2,6 +2,8 @@ package diagnostics
 
 import sources.SourceRange
 
-enum Diagnostic(val msg: String, val sourceRange: SourceRange) {
-  case ExpectedSomething(expected: String, found: String, range: SourceRange) extends Diagnostic(s"Expected '$expected' but found '$found'", range)
+enum Diagnostic(val msg: String, val range: SourceRange) {
+  case UnrecognisedToken(token: String, override val range: SourceRange) extends Diagnostic(s"Unrecognised token '$token'", range)
+  
+  case ExpectedSomething(expected: String, found: String, override val range: SourceRange) extends Diagnostic(s"Expected '$expected' but found '$found'", range)
 }
